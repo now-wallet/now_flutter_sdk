04815a2068562a75c89ea1207bdb20b473aa5ab7
krishpranav <krisna.pranav@gmail.com>
2023-04-28 13:48:04 +0530

    rustbloc: psbt

--- rustbloc/src/psbt.rs
+++ rustbloc/src/psbt.rs
@@ -26,17 +26,71 @@ impl PartiallySignedTransaction {
 
     pub(crate) fn serialize(&self) -> String {
         let psbt = self.internal.lock().unwrap().clone();
-        psbt.to_string();
+        psbt.to_string()
     }
 
     pub(crate) fn txid(&self) -> String {
-        let tx = self.internl.lock().unwrap().clone().extract_tx();
+        let tx = self.internal.lock().unwrap().clone().extract_tx();
         let txid = tx.txid();
-        txid.to_hex();
+        txid.to_hex()
     }
 
-    pub(crate) fn extract_tx(&self) -> &Arc<Transaction> {
+    pub(crate) fn extract_tx(&self) -> Arc<Transaction> {
         let tx = self.internal.lock().unwrap().clone().extract_tx();
         Arc::new(Transaction { internal: tx })
+    }
+
+    pub(crate) fn combine(
+        &self,
+        other: Arc<PartiallySignedTransaction>,
+    ) -> Result<Arc<PartiallySignedTransaction>, BdkError> {
+        let other_psbt = other.internal.lock().unwrap().clone();
+        let mut original_psbt = self.internal.lock().unwrap().clone();
+
+        original_psbt.combine(other_psbt)?;
+        Ok(Arc::new(PartiallySignedTransaction {
+            internal: Mutex::new(original_psbt),
+        }))
+    }
+
+    pub(crate) fn fee_amount(&self) -> Option<u64> {
+        self.internal.lock().unwrap().fee_amount()
+    }
+
+    pub(crate) fn fee_rate(&self) -> Option<Arc<FeeRate>> {
+        self.internal.lock().unwrap().fee_rate().map(Arc::new)
+    }
+}
+
+#[derive(Debug)]
+pub struct Transaction {
+    pub(crate) internal: BdkTransaction,
+}
+
+impl From<RustOpaque<Transaction>> for Transaction {
+    fn from(tx: RustOpaque<Transaction>) -> Self {
+        let vec = tx.serialize();
+        let tx_ = Transaction::new(vec);
+        match tx_ {
+            Ok(e) => e,
+            Err(_) => panic!("Invalid transaction"),
+        }
+    }
+}
+
+impl From<Transaction> for RustOpaque<Transaction> {
+    fn from(tx: Transaction) -> Self {
+        RustOpaque::new(tx)
+    }
+}
+impl Transaction {
+    pub fn new(transaction_bytes: Vec<u8>) -> Result<Self, BdkError> {
+        let mut decoder = Cursor::new(transaction_bytes);
+        let tx: BdkTransaction = BdkTransaction::consensus_decode(&mut decoder)?;
+        Ok(Transaction { internal: tx })
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        self.internal.serialize()
     }
 }
